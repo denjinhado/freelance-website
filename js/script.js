7ddb3222b10d1998fedcfa3059b646728f0fbc2b
phalcobtw <laxz_16@hotmail.com>
2024-05-27 19:54:56 -0700

    navbar lightmode fixed

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 const toggleButton = document.querySelector(".toggleMode");
 const iconLightmode = document.querySelector(".icon-button");
+const navBar = document.querySelector(".nav");
 let isLightMode = false; // Variable para controlar el modo actual
 
 toggleButton.addEventListener("click", () => {
@@ -9,9 +10,11 @@ toggleButton.addEventListener("click", () => {
     body.classList.remove("lightmode");
     isLightMode = false;
     iconLightmode.style.filter = "invert(0%)";
+    navBar.style.backgroundColor = "#030714";
   } else {
     body.classList.add("lightmode");
     isLightMode = true;
     iconLightmode.style.filter = "invert(100%)";
+    navBar.style.backgroundColor = "#ffffff";
   }
 });
