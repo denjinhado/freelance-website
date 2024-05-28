ac9eaa5b75f26757db60e3cbe64f2411539ff049
phalcobtw <laxz_16@hotmail.com>
2024-05-27 20:14:00 -0700

    optimized lightmode

--- js/script.js
+++ js/script.js
@@ -1,6 +1,4 @@
 const toggleButton = document.querySelector(".toggleMode");
-const iconLightmode = document.querySelector(".icon-button");
-const navBar = document.querySelector(".nav");
 let isLightMode = false; // Variable para controlar el modo actual
 
 toggleButton.addEventListener("click", () => {
@@ -9,12 +7,8 @@ toggleButton.addEventListener("click", () => {
   if (isLightMode) {
     body.classList.remove("lightmode");
     isLightMode = false;
-    iconLightmode.style.filter = "invert(0%)";
-    /* navBar.style.backgroundColor = "#030714"; */
   } else {
     body.classList.add("lightmode");
     isLightMode = true;
-    iconLightmode.style.filter = "invert(100%)";
-    /* navBar.style.backgroundColor = "#ffffff"; */
   }
 });
